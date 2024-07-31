05d57db73e99e213fcc7687fc777be6768571628
Sabir-Pathan <pathansbr7@gmail.com>
2024-07-31 19:27:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Cards from "./Component/Cards"
+
+function App() {
+  
+
+  return (
+    <Cards />
+  )
+}
+export default App
---
+++ src/Component/Cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Cards(){
+    return (
+       <div className="h-screen w-full bg-gray-200 flex justify-center items-center">
+
+        <div className="w-80 h-32 bg-gray-50 py-2 px-2">
+            <h1 className="font-bold">Sare Jaha se accha</h1>
+            <h1 className="text-sm mt-3">Sare jaha se accha Hindosta hamara hamara Sare Jahase Accha</h1>
+            <button className="mt-2 mb-4 text-white py-1 px-2 rounded bg-blue-600">Download Now</button>
+
+        </div>
+        
+       </div>
+    )
+}
+
+export default Cards;
