83fcda5c16dec05188bcbe5219b2408c7f20b506
Sabir-Pathan <pathansbr7@gmail.com>
2024-07-31 21:09:04 +0530

    div design

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 function Cards(){
-    let content = [{Name:"Sare Jaha Se Accha" , Decsription:"Sare Jaha Se Accha Hindosta Hamara Hamara" },
+    let data = [{Name:"Sare Jaha Se Accha" , Decsription:"Sare Jaha Se Accha Hindosta Hamara Hamara" },
         {Name:"Hum India Wale" , Decsription:"Sare Jag se Nirale Hum India Wale, Kahete hai log hume India wale" }
-    ];
+    ]
     return (
-<div>
-    {content.map((elem , index) =>(
-        <div className="flex items-center justify-center w-full h-screen bg-zinc-300">
-
-        <div className="w-1/3 min-h-32 bg-zinc-100 rounded">
-
-        </div>
+<div className="h-screen w-full bg-zinc-300 flex items-center justify-center flex-col gap-5">
+    {data.map((item,index)=>(
+        <div className="w-80 h-40 bg-white rounded-md p-6 shadow-md">
+            <h1 className="text-xl font-semibold">{item.Name}</h1>
+            <p className="text-gray-600">{item.Decsription}</p>
+            <button className="bg-blue-700 py-2 px-3 mt-2 mb-2 rounded text-white text-bold">Download Now</button>
         </div>
     ))}
+
 </div>
     )
 }
